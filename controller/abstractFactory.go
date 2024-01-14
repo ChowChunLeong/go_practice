c1225f51ceab00bb75214ac6de1d82e2e923b0ac
chunleong <chunleong@qqtechnology.com.my>
2024-01-14 12:47:08 +0800

    fix no match criteria condition - done

--- controller/abstractFactory.go
+++ controller/abstractFactory.go
@@ -16,10 +16,26 @@ func UseFurniture(c *gin.Context) {
 	if err := c.ShouldBind(&input); err != nil {
 		return
 	}
-	furniture, _ := abstractfactorydesignpattern.CreateFurniture(input.Variant, input.Product)
-	resp := gin.H{
-		"furniture": furniture.SitOn(),
+	furnitureChair, furnitureTable := abstractfactorydesignpattern.CreateFurniture(input.Variant, input.Product)
+	resp := gin.H{}
+	if furnitureChair == nil && furnitureTable == nil {
+		resp = gin.H{
+			"furniture": "no_matched",
+		}
 	}
+
+	if furnitureChair != nil {
+		resp = gin.H{
+			"furniture_chair": furnitureChair.SitOn(),
+		}
+	}
+
+	if furnitureTable != nil {
+		resp = gin.H{
+			"furniture_table": furnitureTable.PutOn(),
+		}
+	}
+
 	helper.SendResponse(c, "furniture", resp)
 
 }
